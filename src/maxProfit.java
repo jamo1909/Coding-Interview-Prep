09db936cb565b507ff67bf338c9622f51487a1cd
jamesmurphy <jmurphwork@gmail.com>
2024-10-24 13:51:54 +1100

    Question Four: Max Stock Profit

---
+++ src/maxProfit.java
@@ -0,0 +1,27 @@
+/**
+ * @Author James Murphy (Jmurph08)
+ *
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ *
+ * Example 1:
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ **/
+public class maxProfit {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
