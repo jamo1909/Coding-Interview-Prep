6f8f2c69f8dcc53ece5906ff0ff01605368ccd50
jamesmurphy <jmurphwork@gmail.com>
2024-10-28 13:22:32 +1100

    Palindrome: Improvement

--- src/isPalindrome.java
+++ src/isPalindrome.java
@@ -29,4 +29,20 @@ public class isPalindrome {
 
         }
     }
+
+    public boolean isPalindromeTwo(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
